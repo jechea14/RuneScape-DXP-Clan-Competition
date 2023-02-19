7104727460527675c9ce3ea72426a8390dca2077
Jeanie Chea <chea.jeanie@gmail.com>
2023-02-19 12:30:42 -0800

    Create model and schema

--- backend/server.js
+++ backend/server.js
@@ -3,26 +3,14 @@ const express = require('express')
 
 const { getPlayerData, getUsernames } = require('./main')
 const app = express()
-// const mongoose = require('mongoose')
+const mongoose = require('mongoose')
 
-// mongoose.connect(process.env.DATABASE_URL)
-// const db = mongoose.connection
-// db.on("error", (error) => console.error(error))
-// db.once('open', () => console.log('Connected to Database'))
-// app.use(express.json())
+mongoose.connect(process.env.DATABASE_URL)
+const db = mongoose.connection
+db.on("error", (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
+app.use(express.json())
 
-// const clanDataSchema = new mongoose.Schema({
-    
-// })
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = process.env.DATABASE_CONNECTION;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
 app.get('/', (request, response) => {
     response.send('hello world!!')
