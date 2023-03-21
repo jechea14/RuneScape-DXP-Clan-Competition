ca79361b150d853b1df28cd365a58a9d60284187
Jeanie Chea <chea.jeanie@gmail.com>
2023-03-21 00:54:05 -0700

    Update brackets.js

--- frontend/pages/brackets.js
+++ frontend/pages/brackets.js
@@ -1,7 +1,39 @@
 import React from "react";
+import axios from "axios";
 
-function brackets() {
+function brackets({ sortData }) {
+  const bracketA = sortData?.filter((user) => user.totalLevelBeforeDxp <= 2000);
+  const bracketB = sortData?.filter(
+    (user) =>
+      user.totalLevelBeforeDxp >= 2001 && user.totalLevelBeforeDxp <= 2300
+  );
+  const bracketC = sortData?.filter(
+    (user) =>
+      user.totalLevelBeforeDxp >= 2301 && user.totalLevelBeforeDxp <= 2600
+  );
+  const bracketD = sortData?.filter(
+    (user) =>
+      user.totalLevelBeforeDxp >= 2601 && user.totalLevelBeforeDxp <= 2700
+  );
+  const bracketE = sortData?.filter(
+    (user) =>
+      user.totalLevelBeforeDxp >= 2701 && user.totalLevelBeforeDxp <= 2800
+  );
+  const bracketF = sortData?.filter(
+    (user) =>
+      user.totalLevelBeforeDxp >= 2801 && user.totalLevelBeforeDxp <= 2850
+  );
+  const bracketG = sortData?.filter((user) => user.totalLevelBeforeDxp >= 2851);
+
   return <div>brackets</div>;
+}
+
+export async function getServerSideProps() {
+  const res = await axios.get("http://localhost:3000/api/data/");
+  const sortData = await res.data.data
+    .sort((a, b) => b.dxpComptotal - a.dxpComptotal)
+    .filter((user) => user.dxpComptotal > 0);
+  return { props: { sortData } };
 }
 
 export default brackets;
