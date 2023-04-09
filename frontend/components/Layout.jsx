0a3a5885891fb482d700f1380dcd4a59234de877
Jeanie Chea <chea.jeanie@gmail.com>
2023-04-08 17:45:48 -0700

    Update Layout.jsx

--- frontend/components/Layout.jsx
+++ frontend/components/Layout.jsx
@@ -1,6 +1,6 @@
 import { Container } from "postcss";
 import React from "react";
-import Footer from "./footer";
+import Footer from "./Footer";
 import Navbar from "./Navbar";
 
 export default function Layout({ children }) {
