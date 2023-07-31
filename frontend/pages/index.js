58a4e8aab5e9064209b4234134995e422a208bc9
Jeanie Chea <chea.jeanie@gmail.com>
2023-07-30 23:41:31 -0700

    Update index.js

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -47,7 +47,7 @@ export default function Home() {
       <main>
         <div className="flex flex-col items-center px-6 md:px-0">
           <h1 className="text-2xl p-6">
-            Double XP Competition Friday July 28th, 2023 - Monday August 7th,
+            Double XP Competition: Friday July 28th, 2023 - Monday August 7th,
             2023
           </h1>
           <h1 className="text-xl p-6">Updates every 2 hours</h1>
