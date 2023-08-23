e1fd092c2aaad214bfc112fe37cf09d88bfb4fbe
Jeanie Chea <chea.jeanie@gmail.com>
2023-08-23 08:19:48 -0700

    Update data.js

--- backend/routes/data.js
+++ backend/routes/data.js
@@ -1,20 +1,16 @@
-const express = require('express')
-require('dotenv').config()
-const router = express.Router()
+const express = require("express");
+require("dotenv").config();
+const router = express.Router();
 const {
-    getAllData,
-    getSingleData,
-    // authMiddleware
-} = require('../controllers/playerDataController')
+  getAllData,
+  getSingleData,
+  // authMiddleware
+} = require("../controllers/playerDataController");
 
 // GET all collections and all data in each collection
-router.get('/', getAllData)
+router.get("/", getAllData);
 
 // GET single player data
-router.get('/player/:id', getSingleData)
+router.get("/player/:id", getSingleData);
 
-// router.get('/api-key', authMiddleware, (req, res) => {
-//     res.status(200).send({ apiKey: process.env.API_KEY })
-// })
-
-module.exports = router
+module.exports = router;
