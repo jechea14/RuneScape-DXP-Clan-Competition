a2ee3bad74c4eaf038770fea53c9c55b4cd296ce
Jeanie Chea <chea.jeanie@gmail.com>
2023-08-26 16:31:57 -0700

    Update Spinner.jsx

--- frontend/components/Spinner.jsx
+++ frontend/components/Spinner.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Loader } from "@mantine/core";
 
+// Loading spinner
 function Spinner() {
   return (
     <div className="flex flex-col justify-center items-center mt-7 space-y-3">
