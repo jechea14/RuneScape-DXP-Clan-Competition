64397af073c8ae68c80742ce6fb7fa785cfe8044
Jeanie Chea <chea.jeanie@gmail.com>
2024-05-15 02:13:05 -0700

    Prepare for Q2 double xp week

--- backend/misc.js
+++ backend/misc.js
@@ -1,12 +1,12 @@
 // Collection names in one place
-const RESULTS_COLLECTION = "dxpresults-02-16-24";
-const SNAPSHOTS_COLLECTION = "snapshots-02-16-24";
-const START_TIME = "2024-02-16 04:00:00";
-const END_TIME = "2024-02-26 04:30:00";
+const RESULTS_COLLECTION = "dxpresults-05-17-24";
+const SNAPSHOTS_COLLECTION = "snapshots-05-17-24";
+const START_TIME = "2024-05-17 05:00:00";
+const END_TIME = "2024-05-27 05:30:00";
 
 module.exports = {
   RESULTS_COLLECTION,
   SNAPSHOTS_COLLECTION,
   START_TIME,
-  END_TIME
+  END_TIME,
 };
