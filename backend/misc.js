4f160517b27148b8678bfd21f92facf53fdadb9c
Jeanie Chea <chea.jeanie@gmail.com>
2026-05-01 02:01:26 -0700

    Update misc.js

--- backend/misc.js
+++ backend/misc.js
@@ -1,8 +1,8 @@
 // Collection names in one place
 const RESULTS_COLLECTION = "dxpresults-5-1-26";
 const SNAPSHOTS_COLLECTION = "snapshots-5-1-26";
-const START_TIME = "2026-5-1 05:00:00";
-const END_TIME = "2026-5-11 05:30:00";
+const START_TIME = "2026-05-01 05:00:00";
+const END_TIME = "2026-05-11 05:30:00";
 
 module.exports = {
   RESULTS_COLLECTION,
